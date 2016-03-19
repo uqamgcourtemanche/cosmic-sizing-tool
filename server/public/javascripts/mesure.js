aa56ded04c84e9fd68f3afa9d6a6c30594d3fa7f
unknown <cd091189@C39024.ens.uqam.ca>
2016-03-19 13:31:45 -0400

    laurier commit

--- server/public/javascripts/mesure.js
+++ server/public/javascripts/mesure.js
@@ -3,33 +3,65 @@ var app = angular.module("myApp", []);
 app.controller("TreeController", ['$scope', function($scope) {
 
     $scope.delete = function(data) {
-        data.nodes = [];
+        data.systems = [];
     };
 
     $scope.addSystem = function(data) {
-        var post = data.nodes.length + 1;
-        var newName = data.name + post;
-        data.nodes.push({
-            name: newName, 
-            mesure:[],
-            nodes: []
+        data.systems.push({
+            name: data.name, 
+            process:[]
         });
     };
 	
-	$scope.addMesure = function(data, mesure){
-		alert(data + " " + mesure);
-		data.mesure.push({
-			groupeDonnee: mesure
-			});
-		console.log(data);
-		console.log(mesure);
-		return false;
+	$scope.addProcess = function(data){
+        data.process.push({
+            name: data.name, 
+            process:[]
+        });
+	};
+	
+	$scope.addDataGroups = function(data, parentProcess){
+		console.log(parentProcess);
+		parentProcess.dataGroups.push({
+			name: data.name,
+			e: !!(data.movement.toUpperCase().indexOf("E")>-1),
+			x: !!(data.movement.toUpperCase().indexOf("X")>-1),
+			r: !!(data.movement.toUpperCase().indexOf("R")>-1),
+			w: !!(data.movement.toUpperCase().indexOf("W")>-1),		
+			commentaire: data.commentaire
+        });
+		data.name = "";
+		data.movement = "";
+		data.commentaire = "";
+	};
+	$scope.returnStartEditing = function(e){
+		if(e.keyCode == 9)
+			event.preventDefault();
+		dataGroupEntery.focus();
 	}
-
-    $scope.tree = [{
-    	name: "test", 
-    	mesure: [], 
-    	nodes: []
-    }];
+    $scope.mesure = [{
+		name: "mesureName",
+		systems : [{
+			name: "systemsName", 
+			process: [{
+				name:"processName",
+				dataGroups: [{
+					name: "dataGroupsName1",
+					e: true,
+					x: true,
+					r: false,
+					w: true,
+					commentaire: "testcommentaire"
+				},{
+					name: "dataGroupsName2",
+					e: false,
+					x: false,
+					r: false,
+					w: false,
+					commentaire: "testcommentaire2"
+				}]
+			}]
+		}]
+	}];
 
 }]);
